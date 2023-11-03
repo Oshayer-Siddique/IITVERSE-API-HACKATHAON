6ce4f351acc2a7a824977c9db79123a5a368707c
Oshayer Siddique <oshayer.siddique2001@gmail.com>
2023-11-04 00:56:50 +0600

    cors

--- Back-end/app.js
+++ Back-end/app.js
@@ -12,6 +12,10 @@ var axios = require('axios');
 
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:3000",
+}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 dotenv.config();
