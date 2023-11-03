e8362fcb357756e61a6b633b27b4e28d669e1123
Oshayer Siddique <oshayer.siddique2001@gmail.com>
2023-11-04 02:11:15 +0600

    post method

--- Back-end/router/router1.js
+++ Back-end/router/router1.js
@@ -6,10 +6,10 @@ const {countrylist,getcoordinate,sortcity,sortpopulation_socialdata,
 sortcityDsc,sortGdp_socialdata} = require('../controllers/controller1');
 
 
-router.get("/countrylist",countrylist);
-router.get("/getdata",getcoordinate);
-router.get("/sortcity",sortcity);
-router.get("/sortcitydsc",sortcityDsc);
-router.get("/sort_population",sortpopulation_socialdata);
-router.get('/sort_gdp',sortGdp_socialdata);
+router.post("/countrylist",countrylist);
+router.post("/getdata",getcoordinate);
+router.post("/sortcity",sortcity);
+router.post("/sortcitydsc",sortcityDsc);
+router.post("/sort_population",sortpopulation_socialdata);
+router.post('/sort_gdp',sortGdp_socialdata);
 module.exports = router;
